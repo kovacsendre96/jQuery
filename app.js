bb4effc578192f08ae71bd412e5efb4e9cee9ec5
Kovács Endre <kovacsendre96@gmail.com>
2020-11-25 13:33:44 +0100

    task8

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 
+// ======TASK8======
+
+$(document).ready(function(){
+  $("div").hover(function(){
+    $(this).animate({left: "500px"}),
+     $(this).animate({left: "0px"})
+    
+  });
+});
 
 
 
+/*
 // ======TASK7======
 $(document).ready(function(){
   $("div").click(function(){
     $(this).css("background-color", "yellow")();
   });
 });
-
-
-/*
 // ======TASK6======
 
 $(document).ready(function(){
