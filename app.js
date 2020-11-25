70c0d93ec43a339beb8f1da0673c62c05f17f6e1
Kovács Endre <kovacsendre96@gmail.com>
2020-11-25 10:36:44 +0100

    task5

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 
+// ======TASK5======
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $(".introvert").hide();
+  });
+});
+
+
+
+
+
+/*
+
+
+
+
+
+
+});
+// ======TASK4======
+$(document).ready(function(){
+  $("button").click(function(){
+    $("#invisible").hide();
+  });
+});
+
+
+// ======TASK3======
+
 
 $(document).ready(function(){
 
   $("#criminal").click(function(){
     $(this).hide();
   });
-
-});
-
-$(document).ready(function(){
-  $("button").click(function(){
-    $("#invisible").hide();
-  });
-});
-   
+  */ 
